e1f108571a2a3be71053af810d409ffc842ca9cb
バース <129232383+surattinon@users.noreply.github.com>
2026-04-16 03:08:41 +0000

    terraform setup

---
+++ azuredeploy.tf
@@ -0,0 +1,29 @@
+# Resource Group
+resource "azurerm_resource_group" "rg1" {
+  name     = var.azure-rg-1
+  location = var.loc1
+}
+
+# VNET
+resource "azurerm_virtual_network" "region1-vnet1-hub1" {
+  name                = var.region1-vnet1-name
+  location            = var.loc1
+  resource_group_name = azurerm_resource_group.rg1.name
+  address_space       = [var.region1-vnet1-address-space]
+}
+
+# ASWA
+resource "azurerm_static_web_app" "nonzaint-aswa1" {
+  name                = var.nonzaint-aswa1
+  resource_group_name = azurerm_resource_group.rg1.name
+  location            = var.loc1
+
+  sku_size = "Free"
+  sku_tier = "Free"
+}
+
+output "deployment_token" {
+  value = azurerm_static_web_app.nonzaint-aswa1.api_key
+  sensitive = true
+  description = "Copy this value into your GitHub Secrets"
+}
